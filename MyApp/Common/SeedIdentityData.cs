64b6aa208566cc9bebae5cb164af536e867206a4
Codehaks <ghods.hakim@gmail.com>
2019-10-11 12:26:17 +0330

    Add project files.

---
+++ MyApp/Common/SeedIdentityData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Common
+{
+    public class SeedIdentityData
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public SeedIdentityData(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task SeedUsers()
+        {
+            var user = await _userManager.FindByNameAsync("admin@myapp.com");
+            if (user == null)
+            {
+
+                await _userManager.CreateAsync(new IdentityUser
+                {
+                    Email = "admin@myapp.com",
+                    UserName = "admin@myapp.com",
+                }, "123@Abc");
+            }
+        }
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MyApp.Common;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+
+                var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                var seedIdentity = new SeedIdentityData(userManager);
+                await seedIdentity.SeedUsers();
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
